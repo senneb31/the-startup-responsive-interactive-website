ed0ba4ad0d47a10cf89656d5eea47cb2dc00be5d
senneb31 <sennebrussel@gmail.com>
2025-01-21 14:00:11 +0100

    mobile first page 2

--- nieuwekijk.js
+++ nieuwekijk.js
@@ -7,3 +7,25 @@ const deNav = document.querySelector("nav");
 function openMenu() {  
   deNav.classList.toggle("nav-open"); 
 }
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.0,
+  };
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      }
+    });
+  }, options);
+  
+  /**
+   * schrijf hier ff op wat alles doet
+   */
+  const meters = document.querySelectorAll("meter");
+  meters.forEach((el) => {
+    observer.observe(el);
+  })
